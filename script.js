2abd58da2cb91670e9f8c87590c98c9edac7875a
Gabe <33893811+Gabefire@users.noreply.github.com>
2023-04-28 14:21:19 -0500

    Created operator functions

---
+++ script.js
@@ -0,0 +1,31 @@
+/* Operator Functions */
+
+function addNums(num1, num2) {
+    finalNum = num1 + num2;
+    return finalNum;
+}
+
+function subtractNums (num1, num2) {
+    finalNum = num1 - num2;
+    return finalNum;
+}
+
+function dividenums (num1, num2) {
+    finalNum = num1 / num2;
+    return finalNum;
+}
+
+function multiplyNums (num1, num2) {
+    finalNum = num1 * num2;
+    return finalNum;
+}
+
+function getPercent (num) {
+    finalNum = num / 100;
+    return finalNum;
+}
+
+function getNegative (num) {
+    finalNum = -1 * num
+    return finalNum
+}
