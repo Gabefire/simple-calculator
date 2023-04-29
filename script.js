229308c3da87b96856d41feae4faacb40ccdc0bc
Gabe <33893811+Gabefire@users.noreply.github.com>
2023-04-28 20:25:46 -0500

    finished basic code

--- script.js
+++ script.js
@@ -21,123 +21,140 @@ function multiplyNums (num1, num2) {
     return finalNum;
 }
 
-function getPercent (num) {
-    finalNum = num / 100;
-    return finalNum;
+function getPercent () {
+    finalNum = Number(display.textContent) / 100;
+    display.textContent = finalNum
 }
 
-function getNegative (num) {
-    finalNum = -1 * num
-    return finalNum
+function getNegative () {
+    finalNum = -1 * Number(display.textContent)
+    display.textContent = finalNum
 }
-// Display
-// Make a blank string concat each input to string
-// if string has a decimal in it disable decimal button
-function getNewDisplay(str) {    
-    display = document.querySelector('.display')
-    nums = document.querySelectorAll('.number')
-    display.textContent = str
-    nums.forEach((num) => {
-        num.addEventListener('click', (e) => {
-        let displayStr = display.textContent
-        if (e.target.id !== "." && displayStr === "0") {displayStr = ""}
-        if (e.target.id === "." && displayStr === "") {displayStr = "0"}
-        displayStr += e.target.id
-        toggleDecimalBtn(displayStr)
-        display.textContent = displayStr
-        return;
-        })
-    })
-}
+// diplay function
+function getDisplay () {
+    if (e.target.id !== "." && displayStr === "0") {displayStr = ""}
+    if (e.target.id === "." && displayStr === "") {displayStr = "0"}
+    displayStr += e.target.id
+    toggleDecimalBtn(displayStr)
+    display.textContent = displayStr
+    }
 
-// toggles decimal button depending if it is in displayStr
+// toggles decimal button depending
 function toggleDecimalBtn(str) {
     if (str.includes(".") || str.length === 0) {
-        document.getElementById("decimal").disabled = true
+        document.getElementById(".").disabled = true
     } else {
-        document.getElementById("decimal").disabled = false
+        document.getElementById(".").disabled = false
     }
 }
 
 // clear value and store value
 
 function storeDisplayValue () {
-    display = document.querySelector('.display')
-    let num1 = Number(display.textContent)
-    getNewDisplay("")
-    return num1
+    num1 = Number(display.textContent)
 }
 
 function clearButton () {
-    display = document.querySelector('.display')
+    document.getElementById(".").disabled = false
     if (num1 === "") {
         display.textContent = "0";
     } 
     else if (display.textContent === "0") {
         return;
     }
-    else {
+    else if (display.textContent === "") {
+        display.textContent = "0"
         num1 = ""
-        getNewDisplay("")
     }
+    else if (num3 !== "") {
+        display.textContent = "0"
+        num1 = ""
+        num2 = ""
+        num3 = ""
     }
-
-//Operator Buttons
-function getFunction () {
-    let functions =  document.querySelectorAll(".op")
-    functions.forEach((func) => {
-        func.addEventListener ("click", (e) => {
-            if (num1 === "") {return;}
-            else {
-                functionOp = e.target.id
-                storeDisplayValue()
-            }
-        })
-    })
+    else {
+        display.textContent = ""
+    }
 }
 
-function evaluateNums (func) {
-    display = document.querySelector('.display')
-    num1 = storeDisplayValue()
-    if (num1 === "" || func === "" || display.textContent === "") {return;}
-    let num2 = display.textContent
-    switch (func) {
+
+
+function evaluateNums () { 
+    if (num1 === "" || functionOp === "" || display.textContent === "" ||
+        num3 !== "") {return;}
+    let num2 = Number(display.textContent)
+    switch (functionOp) {
         case "add":
-            display.textContent = addNums(num1, num2);
+            num3 = addNums(num1, num2);
+            display.textContent = num3;
             num1 = display.textContent;
             break;
         case "subtract":
-            display.textContent = subtractNums(num1, num2);
+            num3 = subtractNums(num1, num2);
+            display.textContent = num3;
             num1 = display.textContent;
             break;
         case "multiply":
-            display.textContent = multiplyNums(num1, num2);
+            num3 = multiplyNums(num1, num2);
+            display.textContent = num3;
             num1 = display.textContent;
             break;
         case "divide":
-            display.textContent = divideNums(num1, num2)
-            num1 = display.textContent
+            num3 = divideNums(num1, num2);
+            display.textContent = num3;
+            num1 = display.textContent;
             break;
     }
 
 }
-getNewDisplay("0")
+/* variable declarations */
 let num1 = ""
+let num2 = ""
+let num3 = ""
 let functionOp = ""  
+let displayStr = "0"
 
+/* display */
+let display = document.querySelector('.display')
+nums = document.querySelectorAll('.number')
+display.textContent = "0"
+nums.forEach((num) => {
+    num.addEventListener('click', (e) => {
+    if (display.textContent.length === 21) {return;}
+    if (num3 !== "") {return;}
+    if (num1 !== "" && displayStr.length === 0) {
+        display.textContent = "";
+        displayStr = "";}
+    displayStr = display.textContent    
+    if (e.target.id !== "." && displayStr === "0") {displayStr = ""}
+    if (e.target.id === "." && displayStr === "") {displayStr = "0"}
+    displayStr += e.target.id
+    toggleDecimalBtn(displayStr)
+    display.textContent = displayStr
+    })
+})
+
+/* Event Listeners */
 let clear = document.querySelector(".clear")
 clear.addEventListener('click', clearButton)
 
 let equal = document.querySelector(".equal")
-equal.addEventListener("click", evaluateNums(functionOp))
+equal.addEventListener("click", evaluateNums)
 
 let functions =  document.querySelectorAll(".op")
 functions.forEach((func) => {
-    func.addEventListener ("click", (e) => {
-        if (num1 === "") {return;}
-        else {
-            functionOp = e.target.id
-            storeDisplayValue()
-        }
+    func.addEventListener ("click", function(event) {
+        functionOp = event.target.id
+        displayStr = ""
+        num3 = ""
+        document.getElementById(".").disabled = false
+        storeDisplayValue()
+        
     })
 })
+
+let negative = document.querySelector("#negative")
+negative.addEventListener("click", getNegative)
+
+let percent = document.querySelector("#percent")
+percent.addEventListener("click", getPercent)
